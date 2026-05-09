1b0c18ff64de5586415c4131575d2a5526870bf4
nwhye <hannaasiadouskaya@gmail.com>
2026-05-09 14:02:23 +0300

    0.1.0 -feat: formulas set up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.text.NumberFormat;
+import java.util.Scanner;
 
+public class Main {
+    static void main() {
+        final byte MONTH_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate:: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR ;
+
+        System.out.println("Period (years): ");
+        byte years = scanner.nextByte();
+        int numbersOfPayments = years * MONTH_IN_YEAR;
+
+        double morgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numbersOfPayments)  / (Math.pow(1 + monthlyInterest, numbersOfPayments) - 1));
+        String morgageFormatted = NumberFormat.getCurrencyInstance().format( morgage );
+        System.out.println("Morgage: " + morgageFormatted);
     }
 }
